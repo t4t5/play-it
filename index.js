40ac248946d538cc8864adac11e03011e2811f7a
Tristan Edwards <tristan.edwards@me.com>
2015-09-08 19:20:23 +0200

    Add first view

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ app.http().io()
  
 //build your realtime-web app 
 console.log("I'm working!");
+
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
  
 app.listen(7076)
